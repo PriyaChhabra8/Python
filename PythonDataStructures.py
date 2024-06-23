08e250243feab54c8f524a37449ab144652c14f2
Priya Chhabra <pcamz1996@gmail.com>
2024-06-23 17:04:33 +0530

    Update

--- PythonDataStructures.py
+++ PythonDataStructures.py
@@ -1,1 +1,24 @@
+Lesson 2: Python Data Structures
 
+Topic 4: Lists
+Explanation:
+Lists are ordered collections that can contain elements of different data types. They are mutable, meaning you can change their contents:
+
+Indexing: Accessing elements in a list using indices.
+Slicing: Extracting sublists from a list.
+
+number = [1,2,3,4,5,6,7,8,9,10]
+fruits = ['Mango','Apple','Banana','Strawberry']
+#Accessing the elements
+print(number[1])
+#slicing
+print(fruits[1:3])
+
+#Create a list of your favorite movies and print the first and last elements.
+
+movies = ['black swan',' Despicable me','Rachel getting married','Inception','Minions']
+print(movies[0])
+print(movies[-1])
+
+#Use slicing to print the second and third elements of the list.
+print(movies[2:4])
