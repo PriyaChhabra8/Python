c95009d0b77c32188e387f3a712a6376d194f4bb
Priya Chhabra <pcamz1996@gmail.com>
2024-06-23 15:36:54 +0530

    Update

--- Basic.py
+++ Basic.py
@@ -1,1 +1,14 @@
 
+# Variables are container to store the data values. Python has several built in data types.
+# int for integer float for floatin pointng number str for string 
+# list ordered collection of items, mutable(can be changed)
+# tuple order collection of items, nonmutable (cannot be changed)
+# dic collection of key value pair
+#set undered collection on unqiue items
+
+# examples
+age = 30
+height = 164.50
+name = 'Alice'
+numbers = [2,4,6,8]
+person = { 'name' : 'Priya', 'age' : 27, 'location' : 'Delhi'} 
