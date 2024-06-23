458bc635c7ed1e4646fcefd5ec8d0459d8f263e1
Priya Chhabra <pcamz1996@gmail.com>
2024-06-23 15:59:51 +0530

    Update

--- Basic.py
+++ Basic.py
@@ -65,3 +65,11 @@ if num % 2 == 0:
     print('Number is even.')
 else:
     print('Number is odd.')
+
+#Use a for loop to iterate through a list and print each item.
+
+numbers1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+for num in numbers1:
+    print(num)
+
+
